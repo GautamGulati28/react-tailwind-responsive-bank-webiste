9177e63564e2bafdde9c863d8d8a0e85159ffdd5
Gautam Gulati <101964337+GautamGulati28@users.noreply.github.com>
2023-08-19 20:43:25 +0530

    updating

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -7,7 +7,7 @@ const Clients = () => (
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) =>(
           <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:win-w-[192px] min-w-[120px]`}>
-            <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain'/>
+            <img src={client.logo} alt="client" className='sm:w-[192px] sm:mb-[10px] w-[100px] object-contain'/>
           </div>
         ))}
       </div>
